242514cfdee57b3a61e6e831d7c405307fb30af5
root <root@localhost.localdomain>
2018-04-30 18:43:06 +0900

    Modify main.c

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 #include "windivert.h"
 
 #define MAXBUF			0xFFFF
-#define IPv4			0x0800
-#define PROTOCOL_TCP	0x06
+#define IPv4			4
+#define PROTOCOL_TCP		6
 
 int main() {
 	typedef struct
@@ -33,7 +33,7 @@ int main() {
 			continue;
 		}
 		PTCPPACKET blocked_pkt = (TCPPACKET*)packet;
-		if( ntohs(blocked_pkt->ip.Version)==IPv4 && ntohs(blocked_pkt->ip.Protocol)==PROTOCOL_TCP &&
+		if(blocked_pkt->ip.Version==IPv4 && blocked_pkt->ip.Protocol==PROTOCOL_TCP &&
 			ntohs(blocked_pkt->tcp.SrcPort)==80 || ntohs(blocked_pkt->tcp.DstPort)==80) {
 			uint8_t ip_buf[INET_ADDRSTRLEN];
 			printf("\nSource IP\t: %s\n", inet_ntop(AF_INET, &blocked_pkt->ip.SrcAddr, ip_buf, INET_ADDRSTRLEN));
