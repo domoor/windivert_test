fbbea314cd191e2de26640d59256a58f4e9645b3
root <root@localhost.localdomain>
2018-04-30 16:56:54 +0900

    Modify main.c

--- main.c
+++ main.c
@@ -7,7 +7,9 @@
 #include <string.h>
 #include "windivert.h"
 
-#define MAXBUF  0xFFFF
+#define MAXBUF			0xFFFF
+#define IPv4			0x0800
+#define PROTOCOL_TCP	0x06
 
 int main() {
 	typedef struct
@@ -30,14 +32,14 @@ int main() {
 			fprintf(stderr, "warning: failed to read packet\n");
 			continue;
 		}
-
-		PTCPPACKET http_pkt = (TCPPACKET*)packet;
-		if(ntohs(http_pkt->tcp.SrcPort)==80 || ntohs(http_pkt->tcp.DstPort)==80) {
+		PTCPPACKET blocked_pkt = (TCPPACKET*)packet;
+		if( ntohs(blocked_pkt->ip.Version)==IPv4 && ntohs(blocked_pkt->ip.Protocol)==PROTOCOL_TCP &&
+			ntohs(blocked_pkt->tcp.SrcPort)==80 || ntohs(blocked_pkt->tcp.DstPort)==80) {
 			uint8_t ip_buf[INET_ADDRSTRLEN];
-			printf("\nSource IP\t: %s\n", inet_ntop(AF_INET, &http_pkt->ip.SrcAddr, ip_buf, INET_ADDRSTRLEN));
-			printf("Destination IP\t: %s\n", inet_ntop(AF_INET, &http_pkt->ip.DstAddr, ip_buf, INET_ADDRSTRLEN));
-			printf("Source Port\t: %u\n", ntohs(http_pkt->tcp.SrcPort));
-			printf("Destination Port: %u\n", ntohs(http_pkt->tcp.DstPort));
+			printf("\nSource IP\t: %s\n", inet_ntop(AF_INET, &blocked_pkt->ip.SrcAddr, ip_buf, INET_ADDRSTRLEN));
+			printf("Destination IP\t: %s\n", inet_ntop(AF_INET, &blocked_pkt->ip.DstAddr, ip_buf, INET_ADDRSTRLEN));
+			printf("Source Port\t: %u\n", ntohs(blocked_pkt->tcp.SrcPort));
+			printf("Destination Port: %u\n", ntohs(blocked_pkt->tcp.DstPort));
 			continue;
 		}
 
