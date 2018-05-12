f0afa26c0bfbbd9eed61713e1a64b1dc3dc2cb1d
root <root@localhost.localdomain>
2018-05-13 00:00:46 +0900

    Modify main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <winsock2.h>
-#include <Ws2tcpip.h>		// inet_ntop()
+#include <Ws2tcpip.h>	// inet_ntop()
 #include <windows.h>
 #include <stdio.h>		// printf()
 #include <stdint.h>		// uintN_t
@@ -9,8 +9,46 @@
 
 #define MAXBUF			0xFFFF
 #define IPv4			4
-#define PROTOCOL_TCP		6
+#define PROTOCOL_TCP	6
 #define PORT_HTTP		80
+
+#define chk_flag		1
+
+struct ipv4_hdr
+{
+	uint8_t HL  : 4,			/* header length */
+			Ver : 4;			/* version */
+	uint8_t tos;				/* type of service */
+	uint16_t len;				/* total length */
+	uint16_t id;				/* identification */
+	uint16_t off;				/* offset */
+	uint8_t ttl;				/* time to live */
+	uint8_t protocol;			/* protocol */
+	uint16_t chk;				/* checksum */
+	struct in_addr src, dst;	/* source and dest address */
+};
+
+struct tcp_hdr
+{
+	uint16_t s_port;			/* source port */
+	uint16_t d_port;			/* destination port */
+	uint32_t seq;				/* sequence number */
+	uint32_t ack;				/* acknowledgement number */
+	uint8_t reservation : 4,	/* (unused) */
+			off         : 4;	/* data offset */
+	uint8_t  flag;				/* control flags */
+	uint16_t windows;			/* window */
+	uint16_t chk;				/* checksum */
+	uint16_t urgent_P;			/* urgent pointer */
+};
+
+void check(struct ipv4_hdr *ip, struct tcp_hdr *tcp) {
+	uint8_t ip_buf[INET_ADDRSTRLEN];
+	printf("\nSource IP\t: %s\n", inet_ntop(AF_INET, &ip->src, ip_buf, INET_ADDRSTRLEN));
+	printf("Destination IP\t: %s\n", inet_ntop(AF_INET, &ip->dst, ip_buf, INET_ADDRSTRLEN));
+	printf("Source Port\t: %u\n", ntohs(tcp->s_port));
+	printf("Destination Port: %u\n", ntohs(tcp->d_port));
+}
 
 int main() {
 	uint16_t priority = 0;
@@ -28,15 +66,11 @@ int main() {
 			continue;
 		}
 
-		WINDIVERT_IPHDR *ip = (WINDIVERT_IPHDR*)packet;
-		if(ip->Version==IPv4 && ip->Protocol==PROTOCOL_TCP) {
-			WINDIVERT_TCPHDR *tcp = (WINDIVERT_TCPHDR*)(packet+(ip->HdrLength)*4);
-			if(ntohs(tcp->SrcPort)==PORT_HTTP || ntohs(tcp->DstPort)==PORT_HTTP) {
-				uint8_t ip_buf[INET_ADDRSTRLEN];
-				printf("\nSource IP\t: %s\n", inet_ntop(AF_INET, &ip->SrcAddr, ip_buf, INET_ADDRSTRLEN));
-				printf("Destination IP\t: %s\n", inet_ntop(AF_INET, &ip->DstAddr, ip_buf, INET_ADDRSTRLEN));
-				printf("Source Port\t: %u\n", ntohs(tcp->SrcPort));
-				printf("Destination Port: %u\n", ntohs(tcp->DstPort));
+		struct ipv4_hdr *ip = (struct ipv4_hdr*)packet;
+		if(ip->Ver==IPv4 && ip->protocol==PROTOCOL_TCP) {
+			struct tcp_hdr *tcp = (struct tcp_hdr*)(packet+(ip->HL<<2));
+			if(ntohs(tcp->s_port)==PORT_HTTP || ntohs(tcp->d_port)==PORT_HTTP) {
+				if(chk_flag) check(ip, tcp);
 				continue;
 			}
 		}
